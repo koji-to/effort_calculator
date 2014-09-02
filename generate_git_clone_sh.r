c761888ca9cc8e8966dd82854599df7690b97eea
Koji Toa <toda@fit.ac.jp>
2014-09-02 17:32:30 +0900

    fix bugs
    Outputted incorrect file list

--- generate_git_clone_sh.r
+++ generate_git_clone_sh.r
@@ -1,9 +1,13 @@
-######## generate .sh script for "git clone"
-##### Set git directory structure file path from chromium project
+####### generate .sh script for "git clone"
+##### Set git directory structure file path
 gitweb<-"https://git.chromium.org/gitweb/?a=project_index"
 #####
 shell.df<-read.csv(gitweb,header=F)
 
+### save git repository tree in local
+write.table(shell.df,"chromium_git_repo_tree.txt",col.names=F,row.names=F,quote=F,append=F)
+
+###generage shell script for "git clone" 
 shell_top.df<-matrix(c("#!/bin/sh","cd git_repo"),2,1)
 write.table(shell_top.df,"git_clone.sh",row.names=F,col.names=F,quote=F,append=F)
 
@@ -12,9 +16,6 @@ write.table(shell.df,"git_clone.sh",row.names=F,col.names=F,quote=F,append=T)
 
 ### make directory for git clone
 dir.create("git_repo")
-
-### save git repository tree in local
-write.table(shell.df,"chromium_git_repo_tree.txt",col.names=F,row.names=F,quote=F,append=F)
 
 ##### This scriot is only for The Chromium Project
 ##### in other(not Chromium) project, run "git clone --recursive <foo>" and "ls -aR | grep \.git:$ | sed -e 's/\.\/\(.*\):/\1/' in top directory
--- generate_git_log_main_sh.r
+++ generate_git_log_main_sh.r
@@ -1,18 +1,18 @@
 ######## generate .sh script for "git log" about main log
 ######## git log option: "%H%ae%ad%ce%cd"
 
-shell.df<-read.csv("chromium_git_repo_tree".txt,header=F)
+shell.df<-read.csv("chromium_git_repo_tree.txt",header=F)
 
 shell_top.df<-matrix(c("#!/bin/sh","logdir=\"`pwd`/git_log_main/\"","hmdir=\"`pwd`\""),3,1)
 write.table(shell_top.df,"git_log_main.sh",row.names=F,col.names=F,quote=F,append=F)
 
 ### replace by regular expression.
 shell_slash.df<-data.frame(shell.df[grep("/",shell.df[,1]),1])
-shell_slash.df[,1]<-gsub("^(.*)\\/(.*)\\.git","cd git_repo\\/\\1\\/\\2\\\nout=\\2\\.git\\\ngit log --date=iso --pretty=format:\"\\[\\[SOF\\]\\]%H<chromium>%ae<chromium>%ad<chromium>%ce<chromium>%cd\\[\\[EOF\\]\\]\" >> \"${logdir}${out}_main.log\"\\\ncd $hmdir",shell_slash.df[,1])
+shell_slash.df[,1]<-gsub("^(.*)\\/(.*)\\.git","cd git_repo\\n\\/\\1\\/\\2\\\nout=\\2\\.git\\\ngit log --date=iso --pretty=format:\"\\[\\[SOF\\]\\]%H<chromium>%ae<chromium>%ad<chromium>%ce<chromium>%cd\\[\\[EOF\\]\\]\" >> \"${logdir}${out}_main.log\"\\\ncd $hmdir",shell_slash.df[,1])
 write.table(shell_slash.df,"git_log_main.sh",row.names=F,col.names=F,quote=F,append=T)
 
 shell_without_slash.df<-data.frame(shell.df[-(grep("/",shell.df[,1])),])
-shell_without_slash.df[,1]<-gsub("^(.*)\\.git","cd git_repo\\/\\1\\\nout=\\1\\.git\\\ngit log --date=iso --pretty=format:\"\\[\\[SOF\\]\\]%H<chromium>%ae<chromium>%ad<chromium>%ce<chromium>%cd\\[\\[EOF\\]\\]\" >> \"${logdir}${out}_main.log\"\\\ncd $hmdir",shell_without_slash.df[,1])
+shell_without_slash.df[,1]<-gsub("^(.*)\\.git","cd git_repo\\n\\/\\1\\\nout=\\1\\.git\\\ngit log --date=iso --pretty=format:\"\\[\\[SOF\\]\\]%H<chromium>%ae<chromium>%ad<chromium>%ce<chromium>%cd\\[\\[EOF\\]\\]\" >> \"${logdir}${out}_main.log\"\\\ncd $hmdir",shell_without_slash.df[,1])
 write.table(shell_without_slash.df,"git_log_main.sh",row.names=F,col.names=F,quote=F,append=T)
 
 dir.create("git_log_main")
