350ae4f1e728d6044e44bf58a8c4a0d16009eb4f
koji-to <toda@fit.ac.jp>
2014-09-03 10:37:38 +0900

    add comment

--- generate_git_clone_sh.r
+++ generate_git_clone_sh.r
@@ -18,4 +18,8 @@ write.table(shell.df,"git_clone.sh",row.names=F,col.names=F,quote=F,append=T)
 dir.create("git_repo")
 
 ##### This scriot is only for The Chromium Project
-##### in other(not Chromium) project, run "git clone --recursive <foo>" and "ls -aR | grep \.git:$ | sed -e 's/\.\/\(.*\):/\1/' in top directory
+##### in other(not Chromium) project,
+##### run "git clone --recursive ..."
+##### and
+##### run "ls -aR | grep \.git:$ | sed -e 's/\.\/\(.*\):/\1/' > chromium_git_repo_tree.txt"
+##### in top directory
