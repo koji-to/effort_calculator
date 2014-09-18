a04290bef08857c229ed242321d20f706990975b
toda <toda@fit.ac.jp>
2014-09-18 18:23:19 +0900

    change formula to calculate "new function commit"

--- calculate_metrics.r
+++ calculate_metrics.r
@@ -29,7 +29,7 @@ for(i in 1:num_release){
   activity.df$commits[i]<-nrow(git_log_tmp.df)
   activity.df$bugfix_commits[i]<-nrow(subset(git_log_tmp.df,git_log_tmp.df$bug_fix==1))
   activity.df$refactoring_commits[i]<-nrow(subset(git_log_tmp.df,git_log_tmp.df$refactoring_flag==1))
-  activity.df$newfunc_commits[i]<-nrow(git_log_tmp.df)-activity.df$bugfix_commits[i]-activity.df$refactoring_commits[i]
+  activity.df$newfunc_commits[i]<-nrow(subset(git_log_tmp.df,git_log_tmp.df$bug_fix!=1 & git_log_tmp.df$refactoring_flag!=1))
   activity.df$add_lines[i]<-sum(git_log_tmp.df$add_lines)
   activity.df$del_lines[i]<-sum(git_log_tmp.df$del_lines)
   activity.df$change_files[i]<-sum(git_log_tmp.df$change_files)
